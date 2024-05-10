0db09605abeec7e39f1ba295b53f9b5c72122c22
ArisTheInquiring <a.Podotas@gmail.com>
2024-05-10 16:47:28 +0300

    fixed a problem with the format string of the email in the output file

--- example.py
+++ example.py
@@ -11,7 +11,7 @@ def main():
 	output_file = rsr.os.path.join("RSR_output_folder", 'Results.txt')
 	generations_output = athens.generations(rate = cmd["-rate"], verbose = cmd["-v"], log = cmd["-log"], drift = cmd["-drift"], criteria = cmd["-criteria"])
 	with open(output_file, 'w') as out_file:
-		out_file.write(f"Reference sequence: {rsr.NCBI_parse(cmd['-acc'], email = "a.Podotas@gmail.com")}\n\nPopulation:\n{generations_output[2]}")
+		out_file.write(f"Reference sequence: {rsr.NCBI_parse(cmd['-acc'], email = 'a.Podotas@gmail.com')}\n\nPopulation:\n{generations_output[2]}")
 		rsr.make_figures(generations_output[0], generations_output[1])
 	print(f"""
 ##############################################################################################################################################
